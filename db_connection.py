3941ab149353882462e1e08eb168b437f04a611d
meriam sikini <128192478+Meriamsikini@users.noreply.github.com>
2025-06-27 22:43:49 +0100

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -2,7 +2,7 @@ import oracledb
 
 def get_connection():
     return oracledb.connect(
-                  user="SYSTEM",
-                    password="VxVk_12()cc",
-                    dsn="localhost:1522/orblvl"
+                  user="",
+                    password="",
+                    dsn=""
     )
